b45bce514180805a7672c4421cd6976b06bd341a
way2he <shiyuan4work@126.com>
2025-03-04 12:03:50 +0800

    生成器

--- 02_function/10_generator.py
+++ 02_function/10_generator.py
@@ -1,1 +1,22 @@
 # 生成器
+g = (x * x for x in range(10))
+
+
+#print(g)
+#print(next(g))
+#print(next(g))
+#print(next(g))
+#print(next(g))
+
+
+# 斐波那契数列
+def fibonacci(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        print(b)
+        a, b = b, a + b
+        n = n + 1
+    print('done')
+
+
+fibonacci(6)
